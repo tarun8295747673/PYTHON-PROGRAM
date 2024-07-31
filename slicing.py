dab4b1ac08109fd6eae5d6461258a6e124de97d8
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:37:24 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,11 @@
+# str[starting index : ending index]
+str1 = 'my name is tarun'
+print(str1[2:8])
+
+str2 ="who are you"
+a = str2[0:7]
+print(a)
+
+# NEGATIVE SLICING
+str3 = "apple"
+print(str3[-4:-1])
