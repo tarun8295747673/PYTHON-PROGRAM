a0b55957a0f521f7b336abdf8f968c58e7853ae6
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:36:29 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,14 @@
+#not operator
+a=10
+b=20
+print("return :",not(a>b))# apposite value if condition is true then it will return false and vice-versa
+
+#and operator
+value1 = True
+value2 = True
+print("return :",value1 and value2)  #both value are true then it will return true
+
+#or operator
+value1 = True
+value2 = True
+print("return :",value1 or value2) #if any ome of the value is true then it will return true
