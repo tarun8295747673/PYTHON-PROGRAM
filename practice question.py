6e0b73c0dcad5fd92130f1325834a487e45a3ba5
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:39:36 +0530

    Add files via upload

---
+++ practice question.py
@@ -0,0 +1,25 @@
+#sum of two number
+
+num1 = int(input("enter num1 :"))
+num2 = int(input("enter num2 :"))
+num3 =num1+num2
+print(num3)
+
+#WAP to print side of a square and print its area
+
+side = int(input("enter the side of a square :"))
+area = side*side
+print(area)
+
+#WAP to input 2 floating number and print their average
+
+num1 = float(input("enter the first floating number :"))
+num2 = float(input("enter the second ffloating number :"))
+average = (num1+num2)/2
+print(average)
+
+#WAP to input 2 number ,a and b print true if a is greater or equal to b. if not print false
+
+num1 = int(input("enter the first number :"))
+num2 = int(input("enter the second number :"))
+print(num1>=num2)
