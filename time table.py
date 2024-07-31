8d845db557b29cb01ec33734d1d9fc7970aaf5ec
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:36:09 +0530

    Add files via upload

---
+++ time table.py
@@ -0,0 +1,15 @@
+day = input("enter the day :")
+if(day== "tarun"):
+    print("very intelligent")
+elif(day== "asmit"):
+    print("intelligent")
+elif(day== "tanuj"):
+    print("great person")
+elif(day== "mohit"):
+    print("deepankar's best friend")
+elif(day== "vikas"):
+    print("bihari")
+elif(day== "deepankar"):
+    print("turtle,frog,duck,all animal's ")
+else:
+    print("intelligent")
