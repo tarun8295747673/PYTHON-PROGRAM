470502c08ab6f8ffb144334964f16a38525158dc
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:39:46 +0530

    Add files via upload

---
+++ practice question3.py
@@ -0,0 +1,10 @@
+# WAP to count the no. of student with the grade "A" in the following
+#["c","d","a","a","w","a","w","a"]
+a = ["c","d","a","a","w","a","w","a"]
+print(a.count("a"))
+
+# WAP to sort the value
+a = ["c","d","a","a","w","a","w","a"]
+a.sort()
+print(a)
+
