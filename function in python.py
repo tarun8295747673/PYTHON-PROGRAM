ad2bfb6ea003e94f43e5c186ab3a76c564643d7e
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:38:40 +0530

    Add files via upload

---
+++ function in python.py
@@ -0,0 +1,28 @@
+#using len function the length of the string
+Name = "tarun kumar"
+print(len(Name))
+
+str1 = "hello my name is tarun"
+len1 = len(str1)
+print(len1)
+
+#using endswith function 
+hi = "i am an engineer"
+print(hi.endswith("eer")) # function checks that the hi string is ending with "eer" or not if yes it will return true  // return true
+
+#using the capitalize function
+str2 = "i am an engineer"
+print(str2.capitalize()) # capitalize 1st char.  // return I am an engineer
+
+#usting replace function
+str3 = "hi i am an engineer"
+print(str3.replace("hi","hello")) #replace hi with hello // return hello i am an engineer
+
+#using find function
+str4 = "hi how are you"
+print(str4.find("are")) #return 1st index no. of 1st accurance // return 7
+
+#using count function
+str5 = " hi hi hi hi"
+print(str5.count("hi")) # count the accourance of the string // return 4
+
