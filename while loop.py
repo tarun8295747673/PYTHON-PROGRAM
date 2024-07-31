3cb43d69c4e1ec06468c42c02c42dde81f97c4d6
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:40:15 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,12 @@
+# a=1
+# while (a <=6):
+#     print(a)
+#     a += 1
+
+
+#to find the factorial of first n number
+n = int(input("enter the number :"))
+fact = 1
+for i in range(1,n+1):
+    fact *=i
+print(fact)
