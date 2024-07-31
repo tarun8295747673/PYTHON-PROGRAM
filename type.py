8324e58419c17f2969416b31e86d2a81c326caea
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:36:43 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,11 @@
+name="tarun"
+age=20
+height=None
+float=22.22
+boolean=True
+print("name is an :",type(name))
+print("age is an :",type(age))
+print("height is an :",type(height))
+print("float is an :",type(float))
+print("boolean is an :",type(boolean))
+
