310326dda2de081350e4644bb0bcb9355fad451b
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-07-31 21:37:00 +0530

    Add files via upload

---
+++ practice question2 .py
@@ -0,0 +1,21 @@
+# WAP to input user's first name and print its length
+
+first_name = input("enter the first name ")
+print(first_name)
+print("the length of",first_name,"is :",len(first_name))
+
+# WAP to check wether the no is odd or even 
+
+num = int(input("enter the number :"))
+if (num%2==0):
+    print("the number is even")
+else:
+    print("the number is odd ")
+
+#WAP to check if a number is multiple of 7 or not'
+
+num2 = int(input("enter the number :"))
+if(num%7==0):
+    print("multiple of 7")
+else:
+    print("not")
