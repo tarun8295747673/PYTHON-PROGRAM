a9b459e1624d47e055aa9a970547e6c237972165
Tarun Kumar <141988770+tarun8295747673@users.noreply.github.com>
2024-08-06 10:17:38 +0530

    Add files via upload

---
+++ find simple interest using function.py
@@ -0,0 +1,8 @@
+def simple_int(p,r,t):
+    return (p*r*t)/100
+
+p = int(input("enter the value of p :"))
+r = int(input("enter the value of r :"))
+t = int(input("enter the value of t :"))
+answer = simple_int(p,r,t)
+print(answer)
